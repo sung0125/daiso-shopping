6db8b5a014d1633db431a1ed4713884aad9e27a4
jsb <sungbae0125@gmail.com>
2024-12-12 14:53:01 +0900

    2nd

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/lib/sample-data';
+import { Product } from '@/types';
+export default function Home() {
+  const validatedProducts: Product[] = sampleData.products.map((product) => ({
+    ...product,
+    isFeatured: product.isFeatured ?? false,
+  }));
+  return (
+    <div className='space-y-8'>
+      <h2 className='h2-bold'>Latest Products</h2>
+      <ProductList data={validatedProducts} />
+    </div>
+  );
+}
